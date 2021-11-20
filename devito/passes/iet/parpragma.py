c0d3615f627611c06cfac40f31bd1d207ef7acbc
George Bisbas <g.bisbas18@imperial.ac.uk>
2021-11-20 21:05:14 +0000

    wp

--- devito/passes/iet/parpragma.py
+++ devito/passes/iet/parpragma.py
@@ -582,11 +582,9 @@ class PragmaDeviceAwareTransformer(DeviceAwareMixin, PragmaShmTransformer):
         return parregion
 
     def _make_nested_partree(self, partree):
-        # Apply heuristic
         if not isinstance(partree.root, self.DeviceIteration):
             return super()._make_nested_partree(partree)
         else:
-            # no-op for now
             if self.nhyperthreads <= self.nested:
                 return partree
 
@@ -619,7 +617,7 @@ class PragmaDeviceAwareTransformer(DeviceAwareMixin, PragmaShmTransformer):
                 if not candidates:
                     continue
 
-                # Introduce nested parallelism
+                # Introduce nested parallelism to outer level
                 subroot, subpartree = self._make_partree(candidates, self.nthreads_nested,
                                                          thread_limit=128)
 
