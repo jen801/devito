2160e6d0506367b2d2274e910efd62332760ea13
George Bisbas <g.bisbas18@imperial.ac.uk>
2022-04-30 23:43:42 +0300

    wip-rebase

--- devito/core/cpu.py
+++ devito/core/cpu.py
@@ -5,8 +5,7 @@ from devito.exceptions import InvalidOperator
 from devito.passes.equations import collect_derivatives
 from devito.passes.clusters import (Lift, blocking, buffering, cire, cse,
                                     extract_increments, factorize, fission, fuse,
-                                    optimize_pows, optimize_msds, optimize_hyperplanes,
-                                    skewing)
+                                    optimize_pows, optimize_hyperplanes, skewing)
 from devito.passes.iet import (CTarget, OmpTarget, avoid_denormals, linearize, mpiize,
                                hoist_prodders, relax_incr_dimensions)
 from devito.tools import timed_pass
