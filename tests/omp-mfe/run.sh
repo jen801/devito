77617a8125a2cc260f71f1a0469b535a80eb8429
FabioLuporini <fabilupo@gmail.com>
2022-06-30 14:10:38 +0200

    aaa

--- tests/omp-mfe/run.sh
+++ tests/omp-mfe/run.sh
@@ -4,4 +4,4 @@ clear
 aompcc -O3 -g -fPIC -Wall -Wno-unused-result -Wno-unused-variable -ffast-math -shared omp_off_leak_mfe.c  -lm -o omp_off_leak_mfe.so
 cp omp_off_leak_mfe.so omp_off_leak_mfe_1.so
 cp omp_off_leak_mfe.so omp_off_leak_mfe_2.so
-python omp_off_leak_mfe_wrapper.py
+python wrapper.py
